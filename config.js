ce2bd71212d785ca26fe00d8952c2f96b6335657
edrylfigz <edrylf@gmail.com>
2026-06-18 04:58:05 +0800

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-const CONFIG = {
-  startSlide: 2,        // always skip title slide
-  endSlide: 15,         // update if slide count changes
-  interval: 6000,       // 6 seconds per slide
-  refreshInterval: 60000 // reload every 60 sec to detect updates
-};
